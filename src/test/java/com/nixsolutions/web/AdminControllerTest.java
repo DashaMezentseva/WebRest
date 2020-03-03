482f521cc9dfda64b552dea3ec3042e0fe1e47a5
Dariya Mezentseva <dariya.mezentseva@nixsolutions.com>
2020-03-03 16:29:13 +0200

    Adding mockito tests

---
+++ src/test/java/com/nixsolutions/web/AdminControllerTest.java
@@ -0,0 +1,133 @@
+package com.nixsolutions.web;
+
+import com.google.code.kaptcha.Constants;
+import com.nixsolutions.domain.Role;
+import com.nixsolutions.domain.User;
+import com.nixsolutions.service.RoleDao;
+import com.nixsolutions.service.UserDao;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.ArrayList;
+
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AdminControllerTest {
+
+    @Mock
+    private UserDao userService;
+    @Mock
+    private RoleDao roleService;
+    @Mock
+    private User user;
+    @Mock
+    private Role role;
+    @InjectMocks
+    private AdminController adminController;
+
+    private MockMvc mockMvc;
+
+    public AdminControllerTest() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/");
+        viewResolver.setSuffix(".jsp");
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(adminController)
+            .setViewResolvers(viewResolver).build();
+    }
+
+    @Test
+    public void testUserList() throws Exception {
+        List<User> userList = mock(ArrayList.class);
+        when(userService.findAll()).thenReturn(userList);
+        mockMvc.perform(get("/admin"))
+            .andExpect(status().isOk())
+            .andExpect(model().attribute("users", userList))
+            .andExpect(view().name("admin"));
+    }
+
+    @Test
+    public void testEditUser() throws Exception {
+        Long id = 2L;
+        when(userService.findById(id)).thenReturn(user);
+        when(roleService.findAll()).thenReturn(new ArrayList<Role>());
+
+        mockMvc.perform(get("/edit").param("userId", String.valueOf(id)))
+            .andExpect(status().isOk())
+            .andExpect(model().attributeExists("roles"))
+            .andExpect(model().attributeExists("userDto"))
+            .andExpect(view().name("edit"));
+    }
+
+
+    @Test
+    public void testAddUser() throws Exception {
+        when(roleService.findAll()).thenReturn(new ArrayList<Role>());
+        mockMvc.perform(get("/add"))
+            .andExpect(status().isOk())
+            .andExpect(model().attributeExists("roles"))
+            .andExpect(model().attributeExists("userDto"))
+            .andExpect(view().name("add"));
+    }
+
+    @Test
+    public void testSubmitAddUserNoErrors() throws Exception {
+        doNothing().when(userService).create(user);
+        mockMvc.perform(post("/add")
+            .param("login", "login")
+            .param("password", "password")
+            .param("passwordAgain", "password")
+            .param("email", "email@gmail.com")
+            .param("firstName", "firstName")
+            .param("lastName", "lastName")
+            .param("birthday", "1990-08-08"))
+            .andExpect(model().size(3))
+            .andExpect(model().hasNoErrors())
+            .andExpect(model().attributeExists("userDto"))
+            .andExpect(model().attributeDoesNotExist("roles"))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("admin?error=user+successfully+created"));
+    }
+
+    @Test
+    public void testSubmitAddUserErrors() throws Exception {
+        doNothing().when(userService).create(user);
+        when(roleService.findAll()).thenReturn(new ArrayList<Role>());
+        when(userService.findByLogin(anyString())).thenReturn(user);
+        when(userService.findByEmail(anyString())).thenReturn(user);
+        mockMvc.perform(post("/add")
+            .param("login", "login")
+            .param("password", "password")
+            .param("passwordAgain", "password1234")
+            .param("email", "email@gmail.com")
+            .param("firstName", "firstName")
+            .param("lastName", "lastName")
+            .param("birthday", "1990-08-08"))
+            .andExpect(model().attributeExists("roles"))
+            .andExpect(model().attributeExists("userDto"))
+            .andExpect(model().attributeErrorCount("userDto", 1))
+            .andExpect(model().attributeHasFieldErrors("userDto", "password"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("add"));
+    }
+
+
+}
--- src/test/java/com/nixsolutions/web/LoginControllerTest.java
+++ src/test/java/com/nixsolutions/web/LoginControllerTest.java
@@ -1,19 +1,26 @@
 package com.nixsolutions.web;
 
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import com.google.code.kaptcha.Constants;
+import com.nixsolutions.domain.Role;
 import com.nixsolutions.domain.User;
+import com.nixsolutions.service.RoleDao;
+import com.nixsolutions.service.UserDao;
 import com.nixsolutions.web.LoginController;
 import java.security.Principal;
 import javax.servlet.http.HttpServletRequest;
@@ -22,8 +29,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,13 +42,20 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @RunWith(MockitoJUnitRunner.class)
 public class LoginControllerTest {
 
+    @Autowired
+    private UserDao userService;
+    @Autowired
+    private RoleDao roleService;
+    @Mock
+    private User user;
+    @Mock
+    private Role role;
+    private static final String CAPTCHA = "Db5g";
+
     @InjectMocks
     private LoginController loginController;
 
     private MockMvc mockMvc;
-
-    private static final String roleAdmin = "ROLE_ADMIN";
-    private static final String roleUser = "ROLE_USER";
 
     public LoginControllerTest() {
     }
@@ -51,50 +67,31 @@ public class LoginControllerTest {
         viewResolver.setSuffix(".jsp");
         MockitoAnnotations.initMocks(this);
         this.mockMvc = MockMvcBuilders.standaloneSetup(loginController)
-                .setViewResolvers(viewResolver).build();
+            .setViewResolvers(viewResolver).build();
     }
 
     @Test
     public void testLogout() throws Exception {
         mockMvc.perform(get("/logout"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("login"));
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("login"));
     }
 
-//    @Test
-//    public void testDispatchAdmin() throws Exception {
-//        HttpServletRequest request = mock(HttpServletRequest.class);
-//        when(request.isUserInRole(roleAdmin)).thenReturn(true);
-//        loginController.dispatch(request);
-//        verify(request, times(1)).isUserInRole(roleAdmin);
-//        verify(request, never()).isUserInRole(roleUser);
-//    }
-//
-//    @Test
-//    public void testDispatchUser() throws Exception {
-//        HttpServletRequest request = mock(HttpServletRequest.class);
-//        when(request.isUserInRole(roleUser)).thenReturn(true);
-//        loginController.dispatch(request);
-//        verify(request, times(1)).isUserInRole(roleAdmin);
-//        verify(request, times(1)).isUserInRole(roleUser);
-//    }
-//
-//    @Test
-//    public void testDispatchIndex() throws Exception {
-//        HttpServletRequest request = mock(HttpServletRequest.class);
-//        when(request.isUserInRole(anyString())).thenReturn(false);
-//        loginController.dispatch(request);
-//        verify(request, times(2)).isUserInRole(anyString());
-//    }
+    @Test
+    public void testLogin() throws Exception {
+        mockMvc.perform(get("/login"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("login"));
+    }
 
 
     @Test
-    public void testIndex() throws Exception {
-        mockMvc.perform(get("/login"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("login"));
+    public void testRegistration() throws Exception {
+        mockMvc.perform(get("/registration"))
+            .andExpect(status().isOk())
+            .andExpect(model().attributeExists("registrationUserDto"))
+            .andExpect(model().attributeDoesNotExist("roles"))
+            .andExpect(view().name("registration"));
     }
-
-
 
 }
